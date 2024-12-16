5bd013ad73af025af60d38a4c3e7117a1377cab2
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-12-16 15:19:04 +0100

    feat : finalizzazione consegna

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import languages from "./data/languages";
+import { useState } from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [activeLanguage, setActiveLanguage] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="container">
+        <div className="text-center">
+          {languages.map((curLanguage, index) => <button className="btn" onClick={() => { setActiveLanguage(curLanguage) }} key={index}>{curLanguage.title}</button>
+          )}
+        </div>
+        <div className="card">
+          <div className="card-title">{ activeLanguage ? activeLanguage.title : "nessun linguaggio selezionato"}</div>
+          <hr />
+          <div className="card-text">{activeLanguage.description}</div>
+        </div>
+
+      </section>
     </>
   )
 }
